d9fb3ef96397e7bb56783599c9a4a1327db8a55c
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2024-06-14 16:31:27 -0400

    refactor: performing validation checks

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"greenlight.example.org/internal/data"
+	"greenlight.example.org/internal/validator"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint. For now simply
@@ -30,6 +31,40 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		// Use the new badRequestResponse() helper.
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	// Use the Check() method to execute our validation checks. This will add the
+	// provided key and error message to the errors map if the check does not evaluate
+	// to true. For example, in the first line here we "check that the title is not
+	// equal to the empty string". in the second, we "check that the length of the title
+	// is less than or equal to 500 bytes" and so on.
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(input.Runtime != 0, "runtime", "must be provided")
+	v.Check(input.Runtime > 0, "runtime", "must be positive integer")
+
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	// note that we're using the Unique helper in the line below to check that all
+	// values in the input.Genres slice are unique
+	v.Check(validator.Unique(input.Genres), "genres", "must not contains duplicate values")
+
+	// Use the Valid() method to see if any of the checks failed. If they did, the use
+	// the failedValidationResponse() helper to send a response to the client, passing
+	// in the v.Errors map.
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
